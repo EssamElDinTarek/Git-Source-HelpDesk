c3c6c3af3f597ee2e85d8a7c275f6c5b80ec8fbe
ahmedMagdyN <ahmed.magdy.noor@gmail.com>
2018-07-19 10:47:06 +0200

    Controllers changes

--- HelpDeskParent/com.sbm.helpdesk.integration/src/main/java/com/sbm/helpdesk/HelpDeskIntegrationAPI/controller/HomeController.java
+++ HelpDeskParent/com.sbm.helpdesk.integration/src/main/java/com/sbm/helpdesk/HelpDeskIntegrationAPI/controller/HomeController.java
@@ -1,153 +1,153 @@
-package com.sbm.helpdesk.HelpDeskIntegrationAPI.controller;
-
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.sbm.helpdesk.HelpDeskIntegrationAPI.restcontroller.RestDTOProvider;
-import com.sbm.helpdesk.common.constant.*;
-import com.sbm.helpdesk.common.dto.ResponseDTO;
-import com.sbm.helpdesk.common.exceptions.types.BusinessException;
-import com.sbm.helpdesk.service.TicketPriorityService;
-import com.sbm.helpdesk.service.TicketService;
-import com.sbm.helpdesk.service.TicketSeverityService;
-import com.sbm.helpdesk.service.WorkflowService;
-import com.sbm.helpdesk.service.dto.BaseDTO;
-import com.sbm.helpdesk.service.dto.TicketDTO;
-import com.sbm.helpdesk.service.dto.TicketPriorityDTO;
-import com.sbm.helpdesk.service.dto.TicketSeverityDTO;
-import com.sbm.helpdesk.service.dto.WorkflowDTO;
-import com.sbm.helpdesk.service.facade.TicketPriorityServiceFacade;
-import com.sbm.helpdesk.service.facade.TicketServiceFacade;
-
-@Controller
-@CrossOrigin("*")
-public class HomeController {
-	
-	
-	@Resource
-	private TicketPriorityServiceFacade facadeService;
-	
-	@Resource
-	private TicketServiceFacade ticketfacadeService;
-	
-	@Resource
-	private TicketService service;
-
-	@Resource
-	private WorkflowService wfservice;
-
-	@Resource
-	private TicketSeverityService ticketSeverityservice;
-
-	@Resource
-	private TicketPriorityService ticketPriorityservice;
-
-	@Resource
-	private RestDTOProvider dtoProvider;
-
-	
-	@RequestMapping(value = "/ticket", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	@ResponseBody
-	public ResponseDTO creatTicket(@RequestParam(IntegrationServicesConstant.PATHPARAM_FILES) MultipartFile[] files, @RequestParam(IntegrationServicesConstant.PATHPARAM_TICKET) String ticket,
-	         Model model) throws BusinessException, Exception {
-		return ticketfacadeService.creatTicket(files, ticket);
-	}
-
-	
-	@RequestMapping(value = "/ticketupdate", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	@ResponseBody
-	public ResponseDTO creatTicketupdate(@RequestParam(IntegrationServicesConstant.PATHPARAM_FILES) MultipartFile[] files, @RequestParam(IntegrationServicesConstant.PATHPARAM_TICKET) String ticket,
-	         Model model) throws BusinessException, Exception {
-		return ticketfacadeService.updateTicket(files, ticket);
-	}
-	@RequestMapping(value = "/ticket", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public ResponseEntity<BaseDTO> updateTicket(@RequestBody TicketDTO ticketdto) {
-		TicketDTO _ticket = service.updateTicket(ticketdto);
-		return dtoProvider.addObj(_ticket);
-	}
-	@RequestMapping(value = "/ticket/{"+IntegrationServicesConstant.TICKET_ID+"}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public ResponseEntity<BaseDTO> deleteTicket(@PathVariable(IntegrationServicesConstant.TICKET_ID) Long ticketId) {
-		service.deleteTicket(ticketId);
-		return null;
-	}
-
-	@RequestMapping(value = "/ticket", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public ResponseEntity<BaseDTO> getTicketByIdentifier(@RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_IDENTIFIER) String key, @RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_VALUE) String value) {
-		TicketDTO _ticket = null;
-		switch (key) {
-		case IntegrationServicesConstant.TICKET_ID:
-			_ticket = service.getByTicketId(Long.parseLong(value));
-			break;
-		case IntegrationServicesConstant.TICKET_NUMBER:
-			_ticket = service.getByTicketNumber(value);
-			break;
-
-		}
-		return dtoProvider.addObj(_ticket);
-	}
-
-	@RequestMapping(value = "/tickets", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public List<TicketDTO> getTiketListByIdentifier(@RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_IDENTIFIER) String key, @RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_VALUE) String value) {
-		List<TicketDTO> _ticketList = null;
-		switch (key) {
-		case IntegrationServicesConstant.PROJECT_NAME:
-			_ticketList = service.getTicketByProjectName(value);
-			break;
-		}
-		return _ticketList;
-	}
-
-	@RequestMapping(value = "/workflow", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public List<WorkflowDTO> getAllWorkflow() {
-		List<WorkflowDTO> _workflowList = wfservice.getAllWorkflow();
-		return _workflowList;
-	}
-
-	@RequestMapping(value = "/ticketseverity", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public List<TicketSeverityDTO> getAllTicketSeverity() {
-		List<TicketSeverityDTO> _ticketSeverityList = ticketSeverityservice.getAllTicketSeverity();
-		return _ticketSeverityList;
-	}
-
-	@RequestMapping(value = "/ticketpriority", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public List<TicketPriorityDTO> getAllTicketPriority() {
-		List<TicketPriorityDTO> _ticketPriorityList = ticketPriorityservice.getAllTicketPriority();
-		return _ticketPriorityList;
-	}
-
-	@RequestMapping(value = "/ticketprioritys", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public ResponseDTO getAllTicketPrioritys() {
-		
-		return facadeService.getAllTicketPriority();
-	}
-	@RequestMapping(value = { "/", "/index" })
-	public ModelAndView index(@RequestParam(required = false, defaultValue = "World") String name) {
-		ModelAndView ret = new ModelAndView("index");
-		// Adds an objet to be used in home.jsp
-		ret.addObject("name", name);
-		return ret;
-	}
-}
+//package com.sbm.helpdesk.HelpDeskIntegrationAPI.controller;
+//
+//import java.util.List;
+//
+//import javax.annotation.Resource;
+//
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import com.sbm.helpdesk.HelpDeskIntegrationAPI.restcontroller.RestDTOProvider;
+//import com.sbm.helpdesk.common.constant.*;
+//import com.sbm.helpdesk.common.dto.ResponseDTO;
+//import com.sbm.helpdesk.common.exceptions.types.BusinessException;
+//import com.sbm.helpdesk.service.TicketPriorityService;
+//import com.sbm.helpdesk.service.TicketService;
+//import com.sbm.helpdesk.service.TicketSeverityService;
+//import com.sbm.helpdesk.service.WorkflowService;
+//import com.sbm.helpdesk.service.dto.BaseDTO;
+//import com.sbm.helpdesk.service.dto.TicketDTO;
+//import com.sbm.helpdesk.service.dto.TicketPriorityDTO;
+//import com.sbm.helpdesk.service.dto.TicketSeverityDTO;
+//import com.sbm.helpdesk.service.dto.WorkflowDTO;
+//import com.sbm.helpdesk.service.facade.TicketPriorityServiceFacade;
+//import com.sbm.helpdesk.service.facade.TicketServiceFacade;
+//
+//@Controller
+//@CrossOrigin("*")
+//public class HomeController {
+//	
+//	
+//	@Resource
+//	private TicketPriorityServiceFacade facadeService;
+//	
+//	@Resource
+//	private TicketServiceFacade ticketfacadeService;
+//	
+//	@Resource
+//	private TicketService service;
+//
+//	@Resource
+//	private WorkflowService wfservice;
+//
+//	@Resource
+//	private TicketSeverityService ticketSeverityservice;
+//
+//	@Resource
+//	private TicketPriorityService ticketPriorityservice;
+//
+//	@Resource
+//	private RestDTOProvider dtoProvider;
+//
+//	
+//	@RequestMapping(value = "/ticket", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//	@ResponseBody
+//	public ResponseDTO creatTicket(@RequestParam(IntegrationServicesConstant.PATHPARAM_FILES) MultipartFile[] files, @RequestParam(IntegrationServicesConstant.PATHPARAM_TICKET) String ticket,
+//	         Model model) throws BusinessException, Exception {
+//		return ticketfacadeService.creatTicket(files, ticket);
+//	}
+//
+//	
+//	@RequestMapping(value = "/ticketupdate", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//	@ResponseBody
+//	public ResponseDTO creatTicketupdate(@RequestParam(IntegrationServicesConstant.PATHPARAM_FILES) MultipartFile[] files, @RequestParam(IntegrationServicesConstant.PATHPARAM_TICKET) String ticket,
+//	         Model model) throws BusinessException, Exception {
+//		return ticketfacadeService.updateTicket(files, ticket);
+//	}
+//	@RequestMapping(value = "/ticket", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public ResponseEntity<BaseDTO> updateTicket(@RequestBody TicketDTO ticketdto) {
+//		TicketDTO _ticket = service.updateTicket(ticketdto);
+//		return dtoProvider.addObj(_ticket);
+//	}
+//	@RequestMapping(value = "/ticket/{"+IntegrationServicesConstant.TICKET_ID+"}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public ResponseEntity<BaseDTO> deleteTicket(@PathVariable(IntegrationServicesConstant.TICKET_ID) Long ticketId) {
+//		service.deleteTicket(ticketId);
+//		return null;
+//	}
+//
+//	@RequestMapping(value = "/ticket", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public ResponseEntity<BaseDTO> getTicketByIdentifier(@RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_IDENTIFIER) String key, @RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_VALUE) String value) {
+//		TicketDTO _ticket = null;
+//		switch (key) {
+//		case IntegrationServicesConstant.TICKET_ID:
+//			_ticket = service.getByTicketId(Long.parseLong(value));
+//			break;
+//		case IntegrationServicesConstant.TICKET_NUMBER:
+//			_ticket = service.getByTicketNumber(value);
+//			break;
+//
+//		}
+//		return dtoProvider.addObj(_ticket);
+//	}
+//
+//	@RequestMapping(value = "/tickets", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public List<TicketDTO> getTiketListByIdentifier(@RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_IDENTIFIER) String key, @RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_VALUE) String value) {
+//		List<TicketDTO> _ticketList = null;
+//		switch (key) {
+//		case IntegrationServicesConstant.PROJECT_NAME:
+//			_ticketList = service.getTicketByProjectName(value);
+//			break;
+//		}
+//		return _ticketList;
+//	}
+//
+//	@RequestMapping(value = "/workflow", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public List<WorkflowDTO> getAllWorkflow() {
+//		List<WorkflowDTO> _workflowList = wfservice.getAllWorkflow();
+//		return _workflowList;
+//	}
+//
+//	@RequestMapping(value = "/ticketseverity", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public List<TicketSeverityDTO> getAllTicketSeverity() {
+//		List<TicketSeverityDTO> _ticketSeverityList = ticketSeverityservice.getAllTicketSeverity();
+//		return _ticketSeverityList;
+//	}
+//
+//	@RequestMapping(value = "/ticketpriority", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public List<TicketPriorityDTO> getAllTicketPriority() {
+//		List<TicketPriorityDTO> _ticketPriorityList = ticketPriorityservice.getAllTicketPriority();
+//		return _ticketPriorityList;
+//	}
+//
+//	@RequestMapping(value = "/ticketprioritys", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+//	@ResponseBody
+//	public ResponseDTO getAllTicketPrioritys() {
+//		
+//		return facadeService.getAllTicketPriority();
+//	}
+//	@RequestMapping(value = { "/", "/index" })
+//	public ModelAndView index(@RequestParam(required = false, defaultValue = "World") String name) {
+//		ModelAndView ret = new ModelAndView("index");
+//		// Adds an objet to be used in home.jsp
+//		ret.addObject("name", name);
+//		return ret;
+//	}
+//}
--- HelpDeskParent/com.sbm.helpdesk.integration/src/main/java/com/sbm/helpdesk/HelpDeskIntegrationAPI/restcontroller/ProjectController.java
+++ HelpDeskParent/com.sbm.helpdesk.integration/src/main/java/com/sbm/helpdesk/HelpDeskIntegrationAPI/restcontroller/ProjectController.java
@@ -1,14 +1,20 @@
 package com.sbm.helpdesk.HelpDeskIntegrationAPI.restcontroller;
 
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.sbm.helpdesk.service.ProjectService;
 import com.sbm.helpdesk.service.dto.*;
 import com.sbm.helpdesk.service.facade.ProjectServiceFacade;
+import com.sbm.helpdesk.common.constant.IntegrationServicesConstant;
 import com.sbm.helpdesk.common.dto.ResponseDTO;
+import com.sbm.helpdesk.common.exceptions.types.BusinessException;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,8 +24,11 @@ public class ProjectController {
 	@Resource
 	private ProjectServiceFacade facadeService;
 	
+	@Resource
+	private ProjectService projectService;
 	
-	
+	@Resource
+	private RestDTOProvider dtoProvider;
 	
 	@RequestMapping(value = "/open", method = RequestMethod.POST, 
 			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -56,4 +65,17 @@ public class ProjectController {
 	public ResponseEntity<List<ProjectDTO>> getAllUsers() {
 		return dtoProvider.getObjList((List) service.listGroups());
 	}*/
+	
+	@RequestMapping(value = "/getProjectById", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<BaseDTO> getProjectById(@RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_VALUE) Long projectId) throws BusinessException {
+		
+		return dtoProvider.getObj((ProjectDTO) projectService.getProjectById(projectId));
+	}
+	
+	@RequestMapping(value = "/getByPortfolioId", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<ProjectDTO>> getProjectByPortfolioId(@RequestParam(IntegrationServicesConstant.SERVICE_RETRIVAL_VALUE) Long portfolioId) throws BusinessException {
+		return dtoProvider.getObjList((List) projectService.getProjectByPortfolioId(portfolioId));
+	}
 }
