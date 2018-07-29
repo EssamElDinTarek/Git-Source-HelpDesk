2743bb79670b3bfcc16a0fac590fb13dea2bbfb7
EssamElDin <essameldintarek@gmail.com>
2018-07-29 17:45:41 +0200

    add mock for the current user object

--- HelpDeskParent/ShuraUIPresentation/src/app/constdata/user.ts
+++ HelpDeskParent/ShuraUIPresentation/src/app/constdata/user.ts
@@ -5,9 +5,9 @@ export const UserData: User = {
   userId: 123,
   firstName: '',
   lastName: '',
-  username: '', 
-  email: '',
-  userPassword: '',
+  email: 'ser@gmail',
+  userPassword: '123456',
+  username: 'ser@gmail',
   portfolio: {portfolioId: 1, name: 'asd', managerLogjn: ''},
   projects: [{projectId: 1, name: 'sbmhelpdesk' }, {projectId: 2, name: 'sbmshura'}]
 };
