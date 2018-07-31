8a4fbfb04bf8526ae06495d7803098b879b9df6b
EssamElDin <essameldintarek@gmail.com>
2018-07-31 11:02:27 +0200

    fix ticket conflict issue

---
+++ HelpDeskParent/ShuraUIPresentation/src/app/fake-db/tickets.ts
@@ -0,0 +1,56 @@
+export class TicketssFakeDb
+{
+
+    description: string;
+	status: string;
+	title: string;
+
+    public static tickets = [
+        {
+            'ticketId'      : '5725a680b3249760ea21de52',
+            'creationdate'    : 'Abbott',
+            'description': 'Keitch',
+            'status'  : 'assets/images/avatars/Abbott.jpg',
+            'title': 'Royalguard',
+            'ticketnumber' : 'Saois'
+        },
+        {
+            'ticketId'      : '5725a680b3249760ea21de52',
+            'creationdate'    : 'Abbott',
+            'description': 'Keitch',
+            'status'  : 'assets/images/avatars/Abbott.jpg',
+            'title': 'Royalguard',
+            'ticketnumber' : 'Saois'
+        },
+        {
+            'id'      : '5725a68009e20d0a9e9acf2a',
+            'name'    : 'Barrera',
+            'lastName': 'Bradbury',
+            'avatar'  : 'assets/images/avatars/Barrera.jpg',
+            'nickname': 'Jackal',
+            'company' : 'Unizim',
+            'jobTitle': 'Graphic Designer',
+            'email'   : 'barrera@withinpixels.com',
+            'phone'   : '+1-202-555-0196',
+            'address' : '183 River Street Passaic, NJ 07055',
+            'birthday': null,
+            'notes'   : ''
+        },
+          {
+            'ticketId'      : '5725a680b3249760ea21de52',
+            'creationdate'    : 'Abbott',
+            'description': 'Keitch',
+            'status'  : 'assets/images/avatars/Abbott.jpg',
+            'title': 'Royalguard',
+            'ticketnumber' : 'Saois'
+        },
+        {
+            'ticketId'      : '5725a680b3249760ea21de52',
+            'creationdate'    : 'Abbott',
+            'description': 'Keitch',
+            'status'  : 'assets/images/avatars/Abbott.jpg',
+            'title': 'Royalguard',
+            'ticketnumber' : 'Saois'
+        }
+    ];
+}
