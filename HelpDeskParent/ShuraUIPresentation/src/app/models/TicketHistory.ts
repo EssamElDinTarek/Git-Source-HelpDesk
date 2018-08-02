2b4f0636058b028eef64895df0189ee29eeaec27
ShoroukNabil <eng.shnabil@gmail.com>
2018-08-02 11:59:13 +0200

    commit after adding mockup files[ticket01_Data.ts-TicketHistory.ts]

---
+++ HelpDeskParent/ShuraUIPresentation/src/app/models/TicketHistory.ts
@@ -0,0 +1,13 @@
+import { FuseUtils } from '@fuse/utils';
+
+export class TicketHistory
+{
+    type: string;
+    actionAt: string;
+    behaviorName: string;
+    behaviorValue: string;
+    id: string;
+    ticketId: string;
+    stepId: string;
+
+}
