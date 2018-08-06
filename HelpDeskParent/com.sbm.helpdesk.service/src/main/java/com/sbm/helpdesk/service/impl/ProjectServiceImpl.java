28a35671c34e0dce38a6756764a43dd794317168
mfaraag <moh_ammar_pro@yahoo.com>
2018-08-06 16:11:37 +0200

    service changes

--- HelpDeskParent/com.sbm.helpdesk.service/src/main/java/com/sbm/helpdesk/service/impl/ProjectServiceImpl.java
+++ HelpDeskParent/com.sbm.helpdesk.service/src/main/java/com/sbm/helpdesk/service/impl/ProjectServiceImpl.java
@@ -9,6 +9,7 @@ import com.sbm.helpdesk.common.exceptions.enums.ExceptionEnums.ExceptionEnums;
 import com.sbm.helpdesk.common.exceptions.types.BusinessException;
 import com.sbm.helpdesk.common.exceptions.types.RespositoryException;
 import com.sbm.helpdesk.service.*;
+import com.sbm.helpdesk.persistence.dao.PortfolioDao;
 import com.sbm.helpdesk.persistence.dao.ProjectDao;
 import com.sbm.helpdesk.persistence.entity.*;
 import com.sbm.helpdesk.common.dto.*;
@@ -17,8 +18,12 @@ import com.sbm.helpdesk.common.dto.*;
 public class ProjectServiceImpl  extends BasicServiceImpl<ProjectDTO, Project>  implements ProjectService {
 
 	@Autowired
-	private ProjectDao projectDao;
+	private ProjectDao dao;
+	
+	@Autowired
+	private PortfolioDao portfoliodao;
 
+	private Project project = new Project();
 	@Override
 	@Transactional
 	public void openProject(ProjectDTO projectDto) {
@@ -26,13 +31,6 @@ public class ProjectServiceImpl  extends BasicServiceImpl<ProjectDTO, Project>  
 		Project project = convertToEntity(bluePrintProject, projectDto);
 		projectDao.persist(project);*/
 		
-	}
-
-	@Override
-	@Transactional
-	public ProjectDTO updateProject(Long projectID) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -49,29 +47,13 @@ public class ProjectServiceImpl  extends BasicServiceImpl<ProjectDTO, Project>  
 		
 	}
 
-	@Override
-	public ProjectDTO getProjectById(Long projectId) throws BusinessException {
-		ProjectDTO result;
-		try {
-			Project project = projectDao.getProjectById(projectId);
-			ProjectDTO projectDTO = new ProjectDTO();
-			result =  convertToDTO(project,projectDTO);
-		}catch(RespositoryException e) {
-			e.printStackTrace();
-			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
-		}
-		catch(Exception e1) {
-			e1.printStackTrace();
-	    	throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
-	    	}
-		 return result;
-	}
+	
 
 	@Override
 	public List<ProjectDTO> getProjectByPortfolioId(Long portfolioId) throws BusinessException {
 		List<ProjectDTO> result;
 		try {
-			List<Project> projects = projectDao.getProjectsByPortfolioId(portfolioId);
+			List<Project> projects = dao.getProjectsByPortfolioId(portfolioId);
 			System.out.println("Hello Test: " + projects.size());
 			result =  projects.stream().
 					map(item -> convertToDTO(item, new ProjectDTO())).collect(Collectors.toList());
@@ -84,5 +66,82 @@ public class ProjectServiceImpl  extends BasicServiceImpl<ProjectDTO, Project>  
 	    	throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
 	    	}
 		 return result;
+	}
+	
+	@Override
+	public ProjectDTO addProject(ProjectDTO projectDTO) throws BusinessException {
+		project = convertToEntity(project, projectDTO);
+		try {
+			project = dao.persist(project);
+		} catch (RespositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return convertToDTO(project, projectDTO);
+	}
+	@Override
+	public ProjectDTO updateProject(ProjectDTO projectDTO) throws BusinessException {
+		try {
+			project = convertToEntity(project, projectDTO);
+			//project.setPortfolio(portfoliodao.findById(project.getPortfolio().getPortfolioId()));
+			project = dao.update(project);
+		
+		} catch (RespositoryException e) {
+			e.printStackTrace();
+			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+		}
+		return  convertToDTO(project, projectDTO);
+	}
+	
+	@Override
+	public String deleteProject(Long id) throws BusinessException {
+		String result = "";
+		try {
+			project= dao.findById(id);
+			project.setDeleted(1);
+		result = "Sucess";
+		}catch(RespositoryException e) {
+			e.printStackTrace();
+			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+		}
+		catch(Exception e1) {
+			e1.printStackTrace();
+	    	throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+	    	}
+		return result;
+	}
+	@Override
+	public ProjectDTO getByProjectId(Long projectId) throws BusinessException {
+		ProjectDTO projectDTO = new ProjectDTO();
+		try {
+			project = dao.findById(projectId);
+			projectDTO = convertToDTO(project, projectDTO);
+		} catch (RespositoryException e) {
+			e.printStackTrace();
+			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+		}
+		return projectDTO;
+	}
+	@Override
+	public List<ProjectDTO> getAllProject() throws BusinessException {
+		List<ProjectDTO> result;
+		try {
+		List<Project> projectList = dao.findAll();
+		result = projectList.stream().map(item -> convertToDTO(item, new ProjectDTO())).collect(Collectors.toList());
+		}catch(RespositoryException e) {
+		e.printStackTrace();
+		throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+	}
+	catch(Exception e1) {
+		e1.printStackTrace();
+    	throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+    	}
+		return result;
 	}
 }
--- HelpDeskParent/com.sbm.helpdesk.service/src/main/java/com/sbm/helpdesk/service/impl/WorkflowServiceImpl.java
+++ HelpDeskParent/com.sbm.helpdesk.service/src/main/java/com/sbm/helpdesk/service/impl/WorkflowServiceImpl.java
@@ -17,18 +17,75 @@ import com.sbm.helpdesk.common.dto.*;
 public class WorkflowServiceImpl extends BasicServiceImpl<WorkflowDTO, Workflow> implements WorkflowService{
 	
 	@Autowired
-	private WorkflowDao workflowDao;
+	private WorkflowDao dao;
+
+	private Workflow workflow = new Workflow();
 	
+	@Override
+	public WorkflowDTO addWorkflow(WorkflowDTO workflowDTO) throws BusinessException {
+		workflow = convertToEntity(workflow, workflowDTO);
+		try {
+			workflow = dao.persist(workflow);
+		} catch (RespositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return convertToDTO(workflow, workflowDTO);
+	}
+	@Override
+	public WorkflowDTO updateWorkflow(WorkflowDTO workflowDTO) throws BusinessException {
+		try {
+			workflow = convertToEntity(workflow, workflowDTO);
+			workflow = dao.update(workflow);
 		
-	public WorkflowServiceImpl() {}
-
-
+		} catch (RespositoryException e) {
+			e.printStackTrace();
+			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+		}
+		return  convertToDTO(workflow, workflowDTO);
+	}
+	
+	@Override
+	public String deleteWorkflow(Long id) throws BusinessException {
+		String result = "";
+		try {
+			workflow= dao.findById(id);
+			workflow.setDeleted(1);
+		result = "Sucess";
+		}catch(RespositoryException e) {
+			e.printStackTrace();
+			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+		}
+		catch(Exception e1) {
+			e1.printStackTrace();
+	    	throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+	    	}
+		return result;
+	}
+	@Override
+	public WorkflowDTO getByWorkflowId(Long workflowId) throws BusinessException {
+		WorkflowDTO workflowDTO = new WorkflowDTO();
+		try {
+			workflow = dao.findById(workflowId);
+			workflowDTO = convertToDTO(workflow, workflowDTO);
+		} catch (RespositoryException e) {
+			e.printStackTrace();
+			throw new BusinessException(ExceptionEnums.REPOSITORY_ERROR);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			throw new BusinessException(ExceptionEnums.BUSINESS_ERROR);
+		}
+		return workflowDTO;
+	}
 	@Override
 	@Transactional
 	public List<WorkflowDTO> getAllWorkflow() throws BusinessException {
 		List<WorkflowDTO> result;
 		try {
-		List<Workflow> workflowList = workflowDao.findAll();
+		List<Workflow> workflowList = dao.findAll();
 		result = workflowList.stream().map(item -> convertToDTO(item, new WorkflowDTO())).collect(Collectors.toList());
 		}catch(RespositoryException e) {
 			e.printStackTrace();
@@ -40,6 +97,5 @@ public class WorkflowServiceImpl extends BasicServiceImpl<WorkflowDTO, Workflow>
 	    	}
 		return result;
 	}
-
 
 }
