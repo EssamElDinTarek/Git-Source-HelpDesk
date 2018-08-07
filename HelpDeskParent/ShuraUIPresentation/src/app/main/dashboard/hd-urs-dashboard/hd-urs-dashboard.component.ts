4826ab7bc078cce2df67ec287cb98d4092ec78f3
ShoroukNabil <eng.shnabil@gmail.com>
2018-08-07 10:34:22 +0200

    Commit after working with user dashboard.

--- HelpDeskParent/ShuraUIPresentation/src/app/main/dashboard/hd-urs-dashboard/hd-urs-dashboard.component.ts
+++ HelpDeskParent/ShuraUIPresentation/src/app/main/dashboard/hd-urs-dashboard/hd-urs-dashboard.component.ts
@@ -16,23 +16,23 @@ import { MainChart } from '../../../models/MainChart';
 
 
 @Component({
-  selector: 'app-hd-urs-dashboard',
-  templateUrl: './hd-urs-dashboard.component.html',
-  styleUrls: ['./hd-urs-dashboard.component.css'],
-  encapsulation: ViewEncapsulation.None,
-  animations   : fuseAnimations
+    selector: 'app-hd-urs-dashboard',
+    templateUrl: './hd-urs-dashboard.component.html',
+    styleUrls: ['./hd-urs-dashboard.component.css'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class HDURSDashboardComponent implements OnInit {
 
     projects: any[];
     selectedProject: any;
-    tickets :Ticket [];
-    workflowList:Workflow[];
+    tickets: Ticket[];
+    workflowList: Workflow[];
 
-    categotyData:MainChart;
-    
+    categoryData: MainChart;
+    selectedCategory: TicketCategory[] = [];
 
-    widgets: any;   
+    widgets: any;
     widget5: any = {};
     widget6: any = {};
     widget7: any = {};
@@ -40,19 +40,19 @@ export class HDURSDashboardComponent implements OnInit {
     widget9: any = {};
     widget11: any = {};
     dateNow = Date.now();
-    chart:any={};
+    chart: any = {};
 
 
 
-     constructor(private _fuseSidebarService: FuseSidebarService,
-    private _ticketService: TicketService,
-    private _projectDashboardService: ProjectDashboardService) {
+    constructor(private _fuseSidebarService: FuseSidebarService,
+        private _ticketService: TicketService,
+        private _projectDashboardService: ProjectDashboardService) {
 
         this.chart = {
-            currentRange : 'status',
-            labels       : true,
-            doughnut     : true,
-            scheme       : {
+            currentRange: 'status',
+            labels: true,
+            doughnut: true,
+            scheme: {
                 domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63']
             }
             /* ,
@@ -61,121 +61,136 @@ export class HDURSDashboardComponent implements OnInit {
             } */
         };
 
-        
-      /**
-         * Widget 5
-         */
+
+        /**
+           * Widget 5
+           */
         this.widget5 = {
-          currentRange  : 'TW',
-          xAxis         : true,
-          yAxis         : true,
-          gradient      : false,
-          legend        : false,
-          showXAxisLabel: false,
-          xAxisLabel    : 'Days',
-          showYAxisLabel: false,
-          yAxisLabel    : 'Isues',
-          scheme        : {
-              domain: ['#42BFF7', '#C6ECFD', '#C7B42C', '#AAAAAA']
-          },
-          onSelect      : (ev) => {
-              console.log(ev);
-          },
-          supporting    : {
-              currentRange  : '',
-              xAxis         : false,
-              yAxis         : false,
-              gradient      : false,
-              legend        : false,
-              showXAxisLabel: false,
-              xAxisLabel    : 'Days',
-              showYAxisLabel: false,
-              yAxisLabel    : 'Isues',
-              scheme        : {
-                  domain: ['#42BFF7', '#C6ECFD', '#C7B42C', '#AAAAAA']
-              },
-              curve         : shape.curveBasis
-          }
-      };
+            currentRange: 'TW',
+            xAxis: true,
+            yAxis: true,
+            gradient: false,
+            legend: false,
+            showXAxisLabel: false,
+            xAxisLabel: 'Days',
+            showYAxisLabel: false,
+            yAxisLabel: 'Isues',
+            scheme: {
+                domain: ['#42BFF7', '#C6ECFD', '#C7B42C', '#AAAAAA']
+            },
+            onSelect: (ev) => {
+                console.log(ev);
+            },
+            supporting: {
+                currentRange: '',
+                xAxis: false,
+                yAxis: false,
+                gradient: false,
+                legend: false,
+                showXAxisLabel: false,
+                xAxisLabel: 'Days',
+                showYAxisLabel: false,
+                yAxisLabel: 'Isues',
+                scheme: {
+                    domain: ['#42BFF7', '#C6ECFD', '#C7B42C', '#AAAAAA']
+                },
+                curve: shape.curveBasis
+            }
+        };
 
-      /**
-       * Widget 6
+        /**
+         * Widget 6
+         */
+        this.widget6 = {
+            currentRange: 'TW',
+            legend: false,
+            explodeSlices: false,
+            labels: true,
+            doughnut: true,
+            gradient: false,
+            scheme: {
+                domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63']
+            },
+            onSelect: (ev) => {
+                console.log(ev);
+            }
+        };
+
+
+
+        /**
+         * Widget 8
+         */
+        this.widget8 = {
+            legend: false,
+            explodeSlices: false,
+            labels: true,
+            doughnut: false,
+            gradient: false,
+            scheme: {
+                domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63', '#ffc107']
+            },
+            onSelect: (ev) => {
+                console.log(ev);
+            }
+        };
+
+        /**
+         * Widget 9
+         */
+        this.widget9 = {
+            currentRange: 'TW',
+            xAxis: false,
+            yAxis: false,
+            gradient: false,
+            legend: false,
+            showXAxisLabel: false,
+            xAxisLabel: 'Days',
+            showYAxisLabel: false,
+            yAxisLabel: 'Isues',
+            scheme: {
+                domain: ['#42BFF7', '#C6ECFD', '#C7B42C', '#AAAAAA']
+            },
+            curve: shape.curveBasis
+        };
+
+
+
+        setInterval(() => {
+            this.dateNow = Date.now();
+        }, 1000);
+
+    }
+
+
+    doSomething(): void {
+        /*    this._ticketService.getCategorizationList().subscribe(_result => {
+               this.categoryData = _result.data;
+             }); */
+
+        if (this.categoryData.priority) {
+            console.log('Priority is selected...!');
+        } else if (this.categoryData.severity) {
+            console.log('Severity is Selected...!');
+        } else if (this.categoryData.status){
+            console.log('Severity is Selected...!');
+        }
+
+    }
+    /**
+       * On init
        */
-      this.widget6 = {
-          currentRange : 'TW',
-          legend       : false,
-          explodeSlices: false,
-          labels       : true,
-          doughnut     : true,
-          gradient     : false,
-          scheme       : {
-              domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63']
-          },
-          onSelect     : (ev) => {
-              console.log(ev);
-          }
-      };
- 
+    ngOnInit(): void {
 
-
-      /**
-       * Widget 8
-       */
-      this.widget8 = {
-          legend       : false,
-          explodeSlices: false,
-          labels       : true,
-          doughnut     : false,
-          gradient     : false,
-          scheme       : {
-              domain: ['#f44336', '#9c27b0', '#03a9f4', '#e91e63', '#ffc107']
-          },
-          onSelect     : (ev) => {
-              console.log(ev);
-          }
-      };
-
-      /**
-       * Widget 9
-       */
-      this.widget9 = {
-          currentRange  : 'TW',
-          xAxis         : false,
-          yAxis         : false,
-          gradient      : false,
-          legend        : false,
-          showXAxisLabel: false,
-          xAxisLabel    : 'Days',
-          showYAxisLabel: false,
-          yAxisLabel    : 'Isues',
-          scheme        : {
-              domain: ['#42BFF7', '#C6ECFD', '#C7B42C', '#AAAAAA']
-          },
-          curve         : shape.curveBasis
-      };
-
-    
-
-      setInterval(() => {
-          this.dateNow = Date.now();
-      }, 1000);
-
-     }
-
-  /**
-     * On init
-     */
-    ngOnInit(): void
-    {
         this._ticketService.getWorkFlowList().subscribe(_result => {
-          this.workflowList = _result.data;
+            this.workflowList = _result.data;
 
         });
 
         this._ticketService.getCategorizationList().subscribe(_result => {
-            this.categotyData = _result.data;
+            this.categoryData = _result.data;
 
-          });
+        });
 
         /* navigator.geolocation.getCurrentPosition(
             
@@ -188,28 +203,28 @@ export class HDURSDashboardComponent implements OnInit {
             }
 
         ); */
-        
+
         this.projects = this._projectDashboardService.projects;
         this.selectedProject = this.projects[0];
         this.widgets = this._projectDashboardService.widgets;
 
-       /*  this._ticketService.getTicketsByProjectID().subscribe(_result => {
-            this.tickets = _result.data;
-            //console.log('Tickets are : '+_result.data);
+        /*  this._ticketService.getTicketsByProjectID().subscribe(_result => {
+             this.tickets = _result.data;
+             //console.log('Tickets are : '+_result.data);
+ 
+           });
+  */
+        /*  this._ticketService.getTicketsByProjectID().subscribe(_result => {
+           this.tickets = _result.data;
+           //console.log('Tickets are : '+_result.data);
 
-          });
- */
-         /*  this._ticketService.getTicketsByProjectID().subscribe(_result => {
-            this.tickets = _result.data;
-            //console.log('Tickets are : '+_result.data);
+         }); */
 
-          }); */
 
-         
 
-     /*      this._ticketService.getWorkflow().subscribe(_workflowlist => {
-            this.workflowList = _workflowlist.data;
-        }); */
+        /*      this._ticketService.getWorkflow().subscribe(_workflowlist => {
+               this.workflowList = _workflowlist.data;
+           }); */
         /**
          * Widget 11
          */
@@ -227,8 +242,7 @@ export class HDURSDashboardComponent implements OnInit {
      *
      * @param name
      */
-    toggleSidebar(name): void
-    {
+    toggleSidebar(name): void {
         this._fuseSidebarService.getSidebar(name).toggleOpen();
     }
 
@@ -240,8 +254,7 @@ export class FilesDataSource extends DataSource<any>
      *
      * @param _widget11
      */
-    constructor(private _widget11)
-    {
+    constructor(private _widget11) {
         super();
     }
 
@@ -250,15 +263,13 @@ export class FilesDataSource extends DataSource<any>
      *
      * @returns {Observable<any[]>}
      */
-    connect(): Observable<any[]>
-    {
+    connect(): Observable<any[]> {
         return this._widget11.onContactsChanged;
     }
 
     /**
      * Disconnect
      */
-    disconnect(): void
-    {
+    disconnect(): void {
     }
 }
